6a3eb3b3f83bb807788e0e2d96fa9dfd486d8956
Joe cooper <swelljoe@gmail.com>
2017-04-22 22:35:44 -0500

    remove noquota from mount options if present

--- lib/Virtualmin/Config/Plugin/Quotas.pm
+++ lib/Virtualmin/Config/Plugin/Quotas.pm
@@ -32,59 +32,62 @@ sub actions {
 
   $self->spin();
   eval {
-  foreign_require("mount", "mount-lib.pl");
-	mkdir("/home", 0755) if (!-d "/home");
-	my ($dir, $dev, $type, $opts) = mount::filesystem_for_dir("/home");
-	mount::parse_options($type, $opts);
-	if (running_in_zone() || &running_in_vserver()) {
-		#print STDERR "Skipping quotas for Vserver or Zones systems\n";
-		return;
-	}
-	elsif ($gconfig{'os_type'} =~ /-linux$/) {
-		$mount::options{'usrquota'} = '';
-		$mount::options{'grpquota'} = '';
-	}
-	elsif ($gconfig{'os_type'} =~ /freebsd|netbsd|openbsd|macos/) {
-		# Skip if quotas are not enabled--requires a kernel rebuild
-		my $quotav = `quota -v`;
-		if ( ! $quotav =~ /none$/ ) {
-			$mount::options{'rw'} = '';
-			$mount::options{'userquota'} = '';
-			$mount::options{'groupquota'} = '';
-		}
-		else {
-			#print "Skipping quotas: Required kernel support is not enabled.\n";
-			return;
-		}
-	}
-	elsif ($gconfig{'os_type'} =~ /solaris/) {
-		$mount::options{'quota'} = '';
-	}
-	else {
-		#print STDERR "Don't know how to enable quotas on $gconfig{'real_os_type'} ($gconfig{'os_type'})\n";
-	}
-	$opts = mount::join_options($type);
-	my @mounts = mount::list_mounts();
-	my $idx;
-	for($idx=0; $idx<@mounts; $idx++) {
-		last if ($mounts[$idx]->[0] eq $dir);
-	}
-	mount::change_mount($idx, $mounts[$idx]->[0],
-			$mounts[$idx]->[1],
-			$mounts[$idx]->[2],
-			$opts,
-			$mounts[$idx]->[4],
-			$mounts[$idx]->[5]);
-	my $err = mount::remount_dir($dir, $dev, $type, $opts);
-	if ($err) {
-		print STDERR "\nThe filesystem $dir could not be remounted with quotas enabled. You may need to reboot your system, and then enable quotas in the Disk Quotas module.\n";
-	}
-	else {
-  # Activate quotas
-		foreign_require("quota", "quota-lib.pl");
-		quota::quotaon($dir, 3);
-	}
-  $self->done(1); # OK!
+    foreign_require("mount", "mount-lib.pl");
+  	mkdir("/home", 0755) if (!-d "/home");
+  	my ($dir, $dev, $type, $opts) = mount::filesystem_for_dir("/home");
+    # Remove noquota, if it is present
+    $opts = join(',', grep {! /noquota/ } (split(/,/, $opts)));
+
+  	mount::parse_options($type, $opts);
+  	if (running_in_zone() || running_in_vserver()) {
+  		#print STDERR "Skipping quotas for Vserver or Zones systems\n";
+  		return;
+  	}
+  	elsif ($gconfig{'os_type'} =~ /-linux$/) {
+  		$mount::options{'usrquota'} = '';
+  		$mount::options{'grpquota'} = '';
+  	}
+  	elsif ($gconfig{'os_type'} =~ /freebsd|netbsd|openbsd|macos/) {
+  		# Skip if quotas are not enabled--requires a kernel rebuild
+  		my $quotav = `quota -v`;
+  		if ( ! $quotav =~ /none$/ ) {
+  			$mount::options{'rw'} = '';
+  			$mount::options{'userquota'} = '';
+  			$mount::options{'groupquota'} = '';
+  		}
+  		else {
+  			#print "Skipping quotas: Required kernel support is not enabled.\n";
+  			return;
+  		}
+  	}
+  	elsif ($gconfig{'os_type'} =~ /solaris/) {
+  		$mount::options{'quota'} = '';
+  	}
+  	else {
+  		#print STDERR "Don't know how to enable quotas on $gconfig{'real_os_type'} ($gconfig{'os_type'})\n";
+  	}
+  	$opts = mount::join_options($type);
+  	my @mounts = mount::list_mounts();
+  	my $idx;
+  	for($idx=0; $idx<@mounts; $idx++) {
+  		last if ($mounts[$idx]->[0] eq $dir);
+  	}
+  	mount::change_mount($idx, $mounts[$idx]->[0],
+  			$mounts[$idx]->[1],
+  			$mounts[$idx]->[2],
+  			$opts,
+  			$mounts[$idx]->[4],
+  			$mounts[$idx]->[5]);
+  	my $err = mount::remount_dir($dir, $dev, $type, $opts);
+  	if ($err) {
+  		print STDERR "\nThe filesystem $dir could not be remounted with quotas enabled. You may need to reboot your system, and then enable quotas in the Disk Quotas module.\n";
+  	}
+  	else {
+    # Activate quotas
+  		foreign_require("quota", "quota-lib.pl");
+  		quota::quotaon($dir, 3);
+  	}
+    $self->done(1); # OK!
   };
   if ($@) {
     $self->done(0);
