2749e6d09cf6991e54d55974adc9827e07be9493
Joe cooper <swelljoe@gmail.com>
2017-05-27 15:58:22 -0500

    Add NTP plugin, sets up ntpd if installed and not KVM

---
+++ lib/Virtualmin/Config/Plugin/NTP.pm
@@ -0,0 +1,49 @@
+package Virtualmin::Config::Plugin::NTP;
+use strict;
+use warnings;
+use 5.010;
+use parent qw(Virtualmin::Config::Plugin);
+my $log = Log::Log4perl->get_logger("virtualmin-config-system");
+
+sub new {
+  my $class = shift;
+
+  # inherit from Plugin
+  my $self = $class->SUPER::new(name => 'NTP');
+
+  return $self;
+}
+
+# actions method performs whatever configuration is needed for this
+# plugin. XXX Needs to make a backup so changes can be reverted.
+sub actions {
+  my $self = shift;
+
+  $self->spin();
+  eval {    # try
+    sleep 1;
+    my $clockfile = "/sys/devices/system/clocksource/clocksource0/current_clocksource";
+    if (-e $clockfile) {
+      open(my $CLOCK, "<", $clockfile) or die "Couldn't open $clockfile: $!";
+      my $clocksource = do { local $/ = <$clocksource> };
+    }
+    if ($clocksource eq "kvm-clock") {
+      $log->info("System clock source is kvm-clock, skipping NTP");
+    }
+    elsif (-x "/usr/sbin/ntpdate-debian") {
+      $self->logsystem("ntpdate-debian");
+    }
+    elsif (-x "/usr/sbin/ntpdate") {
+      $self->logsystem("ntpdate");
+    }
+    # If it's installed, and not kvm-clock, turn ntpd on.
+    if (! $clocksource eq "kvm-clock" && init::action_status("ntpd")) {
+      init::enable_at_boot("ntpd");
+    }
+  } or do {    # catch
+    $self->done(0);    # Something failed
+  };
+  $self->done(1);      # OK!
+}
+
+1;
