2b92cab370ef645d46029daf59674264858bf6b4
Joe Cooper <swelljoe@gmail.com>
2017-07-21 00:12:23 -0500

    Add ns 127.0.0.1 to resolv.conf

--- lib/Virtualmin/Config/Plugin/Extra.pm
+++ lib/Virtualmin/Config/Plugin/Extra.pm
@@ -8,56 +8,63 @@ use parent 'Virtualmin::Config::Plugin';
 use Time::HiRes qw(sleep);
 
 our $config_directory;
-our (%gconfig, %miniserv);
+our ( %gconfig, %miniserv );
 our $trust_unknown_referers = 1;
 
 sub new {
-  my $class = shift;
+    my $class = shift;
 
-  # inherit from Plugin
-  my $self = $class->SUPER::new(
-    name     => 'Extra',
-    depends => [ 'Apache',
-    'Webmin', 'Virtualmin' ]
-  );
+    # inherit from Plugin
+    my $self = $class->SUPER::new(
+        name    => 'Extra',
+        depends => [ 'Apache', 'Webmin', 'Virtualmin' ]
+    );
 
-  return $self;
+    return $self;
 }
 
 # actions method performs whatever configuration is needed for this
 # plugin. XXX Needs to make a backup so changes can be reverted.
 sub actions {
-  my $self = shift;
+    my $self = shift;
 
-  use Cwd;
-  my $cwd  = getcwd();
-  my $root = $self->root();
-  chdir($root);
-  $0 = "$root/virtual-server/config-system.pl";
-  push(@INC, $root);
-  eval 'use WebminCore';    ## no critic
-  init_config();
+    use Cwd;
+    my $cwd  = getcwd();
+    my $root = $self->root();
+    chdir($root);
+    $0 = "$root/virtual-server/config-system.pl";
+    push( @INC, $root );
+    eval 'use WebminCore';    ## no critic
+    init_config();
 
-  $self->spin();
-  sleep 0.2;
-  eval {
-    # Attempt to sync clock
-    if (has_command("ntpdate-debian")) {
-      system("ntpdate-debian >/dev/null 2>&1 </dev/null &");
+    $self->spin();
+    sleep 0.2;
+    eval {
+        # Attempt to sync clock
+        if ( has_command("ntpdate-debian") ) {
+            system("ntpdate-debian >/dev/null 2>&1 </dev/null &");
+        }
+
+        # Turn on caching for downloads by Virtualmin
+        if ( !$gconfig{'cache_size'} ) {
+            $gconfig{'cache_size'} = 50 * 1024 * 1024;
+            $gconfig{'cache_mods'} = "virtual-server";
+            write_file( "$config_directory/config", \%gconfig );
+        }
+
+        # Setup localhost to act as resolver
+
+        open( my $resolv, '+>>', '/etc/resolv.conf' );
+        if ( !grep { /nameserver 127.0.0.1/ } <$resolv> ) {
+            print $resolv 'nameserver 127.0.0.1';
+        }
+        close $resolv;
+
+        $self->done(1);    # OK!
+    };
+    if ($@) {
+        $self->done(0);
     }
-
-    # Turn on caching for downloads by Virtualmin
-    if (!$gconfig{'cache_size'}) {
-      $gconfig{'cache_size'} = 50 * 1024 * 1024;
-      $gconfig{'cache_mods'} = "virtual-server";
-      write_file("$config_directory/config", \%gconfig);
-    }
-
-    $self->done(1);    # OK!
-  };
-  if ($@) {
-    $self->done(0);
-  }
 }
 
 1;
