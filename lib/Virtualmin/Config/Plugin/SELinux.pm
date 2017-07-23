93708d2e59270c26fe51ae9c9b4d1f7beb08547b
Joe Cooper <swelljoe@gmail.com>
2017-07-22 19:55:22 -0500

    Fix syntax in empty SELinux.pm

---
+++ lib/Virtualmin/Config/Plugin/SELinux.pm
@@ -0,0 +1,50 @@
+package Virtualmin::Config::Plugin::SELinux;
+use strict;
+use warnings;
+no warnings qw(once);
+use parent 'Virtualmin::Config::Plugin';
+use Time::HiRes qw( sleep );    # XXX Figure out how to not need this.
+
+our $config_directory;
+our (%gconfig, %miniserv);
+our $trust_unknown_referers = 1;
+
+sub new {
+  my $class = shift;
+
+  # inherit from Plugin
+  my $self = $class->SUPER::new(name => 'SELinux');
+
+  return $self;
+}
+
+# actions method performs whatever configuration is needed for this
+# plugin. XXX Needs to make a backup so changes can be reverted.
+sub actions {
+  my $self = shift;
+  use Cwd;
+  my $cwd  = getcwd();
+  my $root = $self->root();
+  chdir($root);
+  $0 = "$root/virtual-server/config-system.pl";
+  push(@INC, $root);
+  eval 'use WebminCore';    ## no critic
+  init_config();
+
+  if (! -x "/usr/sbin/setsebool") {
+    log->info("SELinux doesn't seem to be installed. Skipping.");
+    return 1;
+  }
+
+  $self->spin();
+  sleep 0.3;    # XXX Useless sleep, prevent spin from ending before it starts
+  eval {
+
+    $self->done(1);    # OK!
+  };
+  if ($@) {
+    $self->done(0);
+  }
+}
+
+1;
