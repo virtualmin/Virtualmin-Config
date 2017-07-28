b670becadc7193f32bd57bdd66adaa9adb1ebfc9
Joe Cooper <swelljoe@gmail.com>
2017-07-27 19:09:39 -0500

    More $total fixes

--- lib/Virtualmin/Config.pm
+++ lib/Virtualmin/Config.pm
@@ -54,11 +54,12 @@ sub run {
   my @plugins = $self->_gather_plugins();
   @plugins = $self->_order_plugins(@plugins);
   my $total = $#plugins;
+  $log->info("Total plugins to be run: $total");
   for (@plugins) {
     my $pkg = "Virtualmin::Config::Plugin::$_";
     load $pkg || die "Loading Plugin failed: $_";
-    my $plugin = $pkg->new( total => $total );
-    $plugin->actions($total);
+    my $plugin = $pkg->new( { total => $total } );
+    $plugin->actions();
     if ($self->{test} && $plugin->can('tests')) {
       $plugin->tests();
     }
