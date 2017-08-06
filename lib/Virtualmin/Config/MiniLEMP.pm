08d66a7875969cb60b0e69ee49490dae06c8f7c6
Joe Cooper <swelljoe@gmail.com>
2017-08-06 08:07:56 -0500

    First check-in of minimal plugin bundles

---
+++ lib/Virtualmin/Config/MiniLEMP.pm
@@ -0,0 +1,37 @@
+package Virtualmin::Config::MiniLEMP;
+use strict;
+use warnings;
+use 5.010_001;
+
+# A list of plugins for configuring a LAMP stack
+
+sub new {
+  my ($class, %args) = @_;
+  my $self = {};
+
+  return bless $self, $class;
+}
+
+sub plugins {
+
+  # Modern system with systemd?
+  if (-x "/usr/bin/systemctl" || -x "/bin/systemctl") {
+    return [
+      "Webmin",  "Nginx",   "Bind",      "Net",
+      "Postfix", "MySQL",   "Firewalld", "Procmail",
+      "ProFTPd", "Quotas",  "Shells",    "Status",
+      "Upgrade", "Usermin", "Webalizer", "Virtualmin",
+      "NTP",     "Fail2banFirewalld"
+    ];
+  }
+  else {
+    return [
+      "Webmin", "Nginx",    "Bind",     "Net",     "Postfix",
+      "MySQL",  "Firewall", "Procmail", "ProFTPd", "Quotas",
+      "Shells", "Status",   "Upgrade",  "Usermin", "Virtualmin",
+      "NTP",    "Fail2ban"
+    ];
+  }
+}
+
+1;
