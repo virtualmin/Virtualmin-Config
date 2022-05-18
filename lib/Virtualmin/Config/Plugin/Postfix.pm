65fa882f1e837a83f627a8d27f22dfa2217c37b9
Ilia <gpg@ilia.engineer>
2022-05-18 09:24:53 +0300

    Revert "Remove `Net` as `Postfix` dependency"
    This reverts commit f8c6e2b06fea2f2ef750f84021ae3210ab6b3086.

--- lib/Virtualmin/Config/Plugin/Postfix.pm
+++ lib/Virtualmin/Config/Plugin/Postfix.pm
@@ -14,7 +14,7 @@ sub new {
   my ($class, %args) = @_;
 
   # inherit from Plugin
-  my $self = $class->SUPER::new(name => 'Postfix', depends => [], %args );
+  my $self = $class->SUPER::new(name => 'Postfix', %args);
 
   return $self;
 }
