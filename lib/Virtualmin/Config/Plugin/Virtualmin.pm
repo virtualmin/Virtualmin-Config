66b6fa3693bec000bf43100944262540cfa8e250
Ilia Ross <ilia@ross.gdn>
2023-08-20 20:17:59 +0300

    Add ability to create host default domain with Let's Encrypt SSL

--- lib/Virtualmin/Config/Plugin/Virtualmin.pm
+++ lib/Virtualmin/Config/Plugin/Virtualmin.pm
@@ -330,6 +330,14 @@ sub actions {
       write_file_contents($profiledphpalias, $phpalias);
     }
 
+    # Try to request SSL certificate for the hostname
+    foreign_require("virtual-server");
+    if (defined(&virtual_server::setup_virtualmin_default_hostname_ssl)) {
+      my ($defdom_status) = virtual_server::setup_virtualmin_default_hostname_ssl();
+      virtual_server::delete_virtualmin_default_hostname_ssl() if (!$defdom_status);
+      mkdir("$ENV{'tempdir'}/virtualmin_ssl_host_success") if ($defdom_status);
+    }
+
     $self->done(1);    # OK!
   };
   if ($@) {
