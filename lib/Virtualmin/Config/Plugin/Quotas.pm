483771c763999ab6004893a9e52a070ee648e8ed
Ilia Ross <ilia@ross.gdn>
2024-10-05 01:40:47 +0300

    Add support for Btrfs quotas
    https://forum.virtualmin.com/t/vmin-install-sh-vs-azure-bastion-ssh/129578/11?u=ilia

--- lib/Virtualmin/Config/Plugin/Quotas.pm
+++ lib/Virtualmin/Config/Plugin/Quotas.pm
@@ -50,6 +50,15 @@ sub actions {
       #print STDERR "Skipping quotas for Vserver or Zones systems\n";
       return;
     }
+    elsif ($type eq 'btrfs') {
+      # Check if Btrfs quotas are already enabled
+      my $quota_status = `btrfs qgroup show $dir 2>&1`;
+      if ($quota_status =~ /ERROR.*not enabled/) {
+          # Enable Btrfs quotas
+          $self->logsystem("btrfs quota enable $dir");
+      }
+      $res = 1;  # Indicate success
+    }
     elsif ($gconfig{'os_type'} =~ /-linux$/) {
       $mount::options{'usrquota'} = '';
       $mount::options{'grpquota'} = '';
@@ -75,106 +84,112 @@ sub actions {
     else {
 #print STDERR "Don't know how to enable quotas on $gconfig{'real_os_type'} ($gconfig{'os_type'})\n";
     }
-    $opts = mount::join_options($type);
-    my @mounts = mount::list_mounts();
-    my $idx;
-    for ($idx = 0; $idx < @mounts; $idx++) {
-      last if ($mounts[$idx]->[0] eq $dir);
+    if ($type eq 'btrfs') {
+        # No need to remount or activate quotas for Btrfs
+        $res = 1;  # Indicate success
     }
-    mount::change_mount(
-      $idx,
-      $mounts[$idx]->[0],
-      $mounts[$idx]->[1],
-      $mounts[$idx]->[2],
-      $opts,
-      $mounts[$idx]->[4],
-      $mounts[$idx]->[5]
-    );
-    my $err = mount::remount_dir($dir, $dev, $type, $opts);
-    if ($type ne "ext4" || $err) {
-      my $xfs   = $type eq 'xfs';
-      my $smsg1 = "\b" x 7 . " " x 7;
-      my $smsg2 = " " x ($xfs ? 26 : 34);
-      my $msg1
-        = "\nThe filesystem $dir could not be remounted with quotas enabled.\n";
-      my $msg2
-        = $xfs
-        ? "You will need to reboot your system to enable quotas."
-        : "You may need to reboot your system, and/or enable quotas\nmanually in Webmin/System/Disk Quotas module.";
-      $res = 2;
-      my $prt_std_err = 1;
-      if ($xfs) {
+    else {
+      $opts = mount::join_options($type);
+      my @mounts = mount::list_mounts();
+      my $idx;
+      for ($idx = 0; $idx < @mounts; $idx++) {
+        last if ($mounts[$idx]->[0] eq $dir);
+      }
+      mount::change_mount(
+        $idx,
+        $mounts[$idx]->[0],
+        $mounts[$idx]->[1],
+        $mounts[$idx]->[2],
+        $opts,
+        $mounts[$idx]->[4],
+        $mounts[$idx]->[5]
+      );
+      my $err = mount::remount_dir($dir, $dev, $type, $opts);
+      if ($type ne "ext4" || $err) {
+        my $xfs   = $type eq 'xfs';
+        my $smsg1 = "\b" x 7 . " " x 7;
+        my $smsg2 = " " x ($xfs ? 26 : 34);
+        my $msg1
+          = "\nThe filesystem $dir could not be remounted with quotas enabled.\n";
+        my $msg2
+          = $xfs
+          ? "You will need to reboot your system to enable quotas."
+          : "You may need to reboot your system, and/or enable quotas\nmanually in Webmin/System/Disk Quotas module.";
+        $res = 2;
+        my $prt_std_err = 1;
+        if ($xfs) {
 
-        my $grubby_cmd = &has_command('grubby');
-        my $grub_def_file = "/etc/default/grub";
-        my $grub_generate_config = sub {
-            my $grub_conf_file = "/boot/grub2/grub.cfg";
-            my $grub_conf_cmd = "grub2-mkconfig";
-            if (!-r $grub_conf_file) {
-              $grub_conf_file = "/boot/grub/grub.cfg";
-              $grub_conf_cmd = "grub-mkconfig";
-            }
-            # On EFI it's different config file
-            if (-d "/sys/firmware/efi") {
-              my %osrelease;
-              &read_env_file('/etc/os-release', \%osrelease);
-              my $osid               = $osrelease{'ID'};
-              my $grub_conf_file_efi = "/boot/efi/EFI/$osid/grub.cfg";
-              if (-r $grub_conf_file_efi) {
-                $grub_conf_file = $grub_conf_file_efi;
+          my $grubby_cmd = &has_command('grubby');
+          my $grub_def_file = "/etc/default/grub";
+          my $grub_generate_config = sub {
+              my $grub_conf_file = "/boot/grub2/grub.cfg";
+              my $grub_conf_cmd = "grub2-mkconfig";
+              if (!-r $grub_conf_file) {
+                $grub_conf_file = "/boot/grub/grub.cfg";
+                $grub_conf_cmd = "grub-mkconfig";
               }
-            }
-            if (-r $grub_conf_file) {
-              &copy_source_dest($grub_conf_file, "$grub_conf_file.orig");
-              $self->logsystem("$grub_conf_cmd -o $grub_conf_file");
-            }
-        };
-        
-        # Use grubby command to enable user and group quotas
-        if (-x $grubby_cmd) {
-          $self->logsystem(
-            "$grubby_cmd --update-kernel=ALL --args=rootflags=uquota,gquota"
-          );
-          # Generate a new actual config file
-          &$grub_generate_config();
-        }
-        # Update configuration manually
-        elsif (-r $grub_def_file) {
-          my %grub;
-          &read_env_file($grub_def_file, \%grub) || ($res = 0);
-          my $v = $grub{'GRUB_CMDLINE_LINUX'};
-          if (defined($v) && $v !~ /rootflags=.*?([u|g]quota)/) {
-            if ($v =~ /rootflags=(\S+)/) {
-              $v =~ s/rootflags=(\S+)/rootflags=$1,uquota,gquota/;
-            }
-            else {
-              $v .= " rootflags=uquota,gquota";
-            }
-            $grub{'GRUB_CMDLINE_LINUX'} = $v;
-            &write_env_file($grub_def_file, \%grub);
-
+              # On EFI it's different config file
+              if (-d "/sys/firmware/efi") {
+                my %osrelease;
+                &read_env_file('/etc/os-release', \%osrelease);
+                my $osid               = $osrelease{'ID'};
+                my $grub_conf_file_efi = "/boot/efi/EFI/$osid/grub.cfg";
+                if (-r $grub_conf_file_efi) {
+                  $grub_conf_file = $grub_conf_file_efi;
+                }
+              }
+              if (-r $grub_conf_file) {
+                &copy_source_dest($grub_conf_file, "$grub_conf_file.orig");
+                $self->logsystem("$grub_conf_cmd -o $grub_conf_file");
+              }
+          };
+          
+          # Use grubby command to enable user and group quotas
+          if (-x $grubby_cmd) {
+            $self->logsystem(
+              "$grubby_cmd --update-kernel=ALL --args=rootflags=uquota,gquota"
+            );
             # Generate a new actual config file
             &$grub_generate_config();
           }
-          else {
-            $res         = 1;
-            $prt_std_err = 0;
+          # Update configuration manually
+          elsif (-r $grub_def_file) {
+            my %grub;
+            &read_env_file($grub_def_file, \%grub) || ($res = 0);
+            my $v = $grub{'GRUB_CMDLINE_LINUX'};
+            if (defined($v) && $v !~ /rootflags=.*?([u|g]quota)/) {
+              if ($v =~ /rootflags=(\S+)/) {
+                $v =~ s/rootflags=(\S+)/rootflags=$1,uquota,gquota/;
+              }
+              else {
+                $v .= " rootflags=uquota,gquota";
+              }
+              $grub{'GRUB_CMDLINE_LINUX'} = $v;
+              &write_env_file($grub_def_file, \%grub);
+
+              # Generate a new actual config file
+              &$grub_generate_config();
+            }
+            else {
+              $res         = 1;
+              $prt_std_err = 0;
+            }
           }
         }
+        if ($prt_std_err && $res) {
+          print $smsg1;
+          print $msg1;
+          print $msg2;
+          print $smsg2;
+        }
       }
-      if ($prt_std_err && $res) {
-        print $smsg1;
-        print $msg1;
-        print $msg2;
-        print $smsg2;
+      else {
+        # Activate quotas
+        $self->logsystem("modprobe quota_v2");
+        $self->logsystem("quotacheck -vgum $dir");
+        $self->logsystem("quotaon -av");
+        $res = 1;
       }
-    }
-    else {
-      # Activate quotas
-      $self->logsystem("modprobe quota_v2");
-      $self->logsystem("quotacheck -vgum $dir");
-      $self->logsystem("quotaon -av");
-      $res = 1;
     }
     $self->done($res);    # Maybe OK!
   };
